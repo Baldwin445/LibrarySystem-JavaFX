b2045160a866997f78436bd98aabb8d75b0222a5
baldwin722 <baldwin722@163.com>
2019-10-18 22:24:20 +0800

    提交图标图片
    仅能使用png图片做图标

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("图书馆管理系统");
+        primaryStage.setScene(new Scene(root, 1500, 875));
+        primaryStage.getIcons().add(new Image("/image/system_icon_128px.png"));
+
+        //hide the Title
+        //primaryStage.initStyle(StageStyle.TRANSPARENT);
+
         primaryStage.show();
     }
 
