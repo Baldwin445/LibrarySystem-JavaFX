cd9b8279907e1c489ac67cfb38bc28fd80a0b78f
jinl <jinl1874@foxmail.com>
2019-11-11 23:14:46 +0800

    修复首页的bug

--- src/user/HomepageController.java
+++ src/user/HomepageController.java
@@ -4,14 +4,18 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import properties.Property;
 
+import java.sql.SQLException;
+
 
 public class HomepageController {
 
     @FXML
     private Label username;
     @FXML
-    private void initialize() {
-        String name = Property.getKeyValue("name");
+    private void initialize() throws SQLException {
+        UserData user = new UserData(Property.getKeyValue("ID"));
+        String name = user.getName();
+        Property.updateProperties("name", name);
         username.setText(name);
     }
 }
